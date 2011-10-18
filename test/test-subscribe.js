3fe2b5e8e023b98d5c4c4fa9331c3694dd7b5a11
Orlando Vazquez <ovazquez@gmail.com>
2011-10-18 12:08:33 -0700

    Add test and package.json files

---
+++ test/test-subscribe.js
@@ -0,0 +1,24 @@
+require.paths.push(__dirname+'/../lib');
+var sysevent = require('sysevent');
+
+exports["test subscribing to all events"]
+= function (test) {
+  var sub = new sysevent.Subscription();
+
+  sub.on('event', function (event) {
+    console.log("Got a sysevent:");
+    console.dir(event);
+    test.equal(event.class, 'xxx', 'class name should match');
+    test.equal(event.subclass, 'yyy', 'subclass name should match');
+    test.equal(event.vendor, 'joyent', 'vendor name should match');
+    test.equal(event.pid, process.pid, 'process pid name should match');
+    test.equal(event.pub, process.argv[1], 'publisher name should match');
+    sub.unsubscribe();
+    test.done();
+  });
+
+  sub.subscribe("xxx", "subclass1");
+
+  var event = { key1: 'value' };
+  sub.post("xxx", "yyy", "joyent", process.argv[1], event)
+}
